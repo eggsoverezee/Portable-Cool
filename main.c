e7ff585f901f68eb13915a7738712ce5d4cbc33b
emmanuel braxton <ebraxton@worcester.edu>
2023-06-24 23:03:10 +0000

    Replace main.c

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
  #include <cool_tracking.c>
  
  int main(){
-   	create_connect();
-   	camera_open();
+    create_connect();
+    camera_open();
     graphics_open(160, 120);
-  	thread camThread = thread_create(camOps);
+    thread camThread = thread_create(camOps);
     thread driveThread = thread_create(driveOps);
     thread_start(camThread);
     thread_start(driveThread);
 
     while(1){
         if(STOPPAGE == 1){
-         	thread_destroy(camThread);
+            thread_destroy(camThread);
             thread_destroy(driveThread);
             break;
         }
         else{
-        	thread_wait(camThread);
-        	thread_wait(driveThread);
+            thread_wait(camThread);
+            thread_wait(driveThread);
         }
     }
 
     camera_close();
-	create_disconnect();
+    create_disconnect();
     return 0;
 }
